ecf2243056e8a81a148a798e07b614935c105dcc
kukinpower <thekukinpower@gmail.com>
2020-11-22 21:41:53 +0300

    add comments to rot13

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,3 +1,6 @@
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/caesars-cipherhttps://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/caesars-cipher
+
 function rot13(str) {
 	let arr = str.split("");
 	for (let i = 0; i < str.length; i++) {
